d117b86768b98c3c46c89f3d493c4322cdcd0518
Carlos Chininin <carloschininin@gmail.com>
2025-03-14 22:31:38 -0500

    add graphics amcharts demo

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,20 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $data = [
+            ['country' => 'USA', "value" => rand(500, 2000)],
+            ['country' => 'CHINA', "value" => rand(500, 2000)],
+            ['country' => 'ALEMANIA', "value" => rand(500, 2000)],
+            ['country' => 'FRANCIA', "value" => rand(500, 2000)],
+            ['country' => 'RUSIA', "value" => rand(500, 2000)],
+            ['country' => 'CANADA', "value" => rand(500, 2000)],
+            ['country' => 'MEXICO', "value" => rand(500, 2000)],
+            ['country' => 'PERU', "value" => rand(500, 2000)],
+        ];
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'dataPaises' => $data
         ]);
     }
 }
